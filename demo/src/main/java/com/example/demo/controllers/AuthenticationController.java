d939e8e17a52e2694bc33ad4520a947fed405faf
ilham-jalal <jalalilham55@gmail.com>
2024-09-27 15:04:34 +0100

    Quality Gate Check v7

--- demo/src/main/java/com/example/demo/controllers/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
